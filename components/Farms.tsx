c668c55f9181c71e3afba5c92bf56b8d73eabc30
pryority <matthewapryor@gmail.com>
2022-12-12 20:10:01 -0500

    fix hydration error

--- components/Farms.tsx
+++ components/Farms.tsx
@@ -18,8 +18,10 @@ export const Farms = () => {
   return (
     <section className='flex flex-col items-center space-y-8 py-8'>
       <div className='grid grid-cols-4 w-full justify-center'>
-          {state.items.slice(0, state.visible).map((farm: any, i:any) =>(
-              <div key={i} className="flex relative p-2">
+        {
+          state.items.slice(0, state.visible)
+          .map((farm: any, i:any) =>(
+            <div key={i} className="flex relative p-2">
               <Link
               href={`https://${farm.url}`}
               className="flex flex-col w-56 transition-all ease-in-out duration-100 visited:text-sky-900 bg-lime-50-500 hover:bg-lime-300 rounded-xl border p-6 text-left hover:text-lime-900 focus:text-lime-50 focus:bg-lime-500"
@@ -27,12 +29,13 @@ export const Farms = () => {
                 <h3 className="text-2xl font-bold">{farm.name}</h3>
                 <h2>{farm.symbol}</h2>
               </Link>
-              </div>
-            ))}
+            </div>
+          ))
+        }
       </div>
       {state.visible < state.items.length &&
         <button onClick={loadMore} type="button" className="load-more bg-lime-400 rounded-lg relative justify-center flex w-full">
-          <p className='hover:scale-[1.03] transition-all ease-in-out duration-100 uppercase tracking-widest hover:mt-[1px] rounded-lg border border-lime-500 text-lime-900 absolute top-0 flex px-4 bg-lime-400 py-2'>Load more</p>
+          <p className='transition-all ease-in-out duration-100 uppercase tracking-widest hover:mt-[1px] rounded-lg border border-lime-500 text-lime-900 absolute top-0 flex px-4 bg-lime-400 hover:bg-lime-500 hover:text-lime-50 focus:bg-lime-900 py-2'>Load more</p>
         </button>
       }
     </section>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
@@ -5,15 +6,9 @@ type Props = {}
 export const Footer = (props: Props) => {
   return (
     <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by IPFS
-          {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
-        </a>
+        <div className="flex items-center justify-center gap-2 text-slate-600">
+          Powered by <Link href={"https://ens.domains/"} className="text-sky-600 font-bold hover:text-sky-800 transition-all ease-in-out duration-200">IPFS</Link>, <Link href={"https://ens.domains/"} className="text-sky-600 font-bold hover:text-sky-800 transition-all ease-in-out duration-200">ENS</Link>
+        </div>
       </footer>
   )
 }
