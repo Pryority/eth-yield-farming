4a98df6252c0d7499f1c49371f7bd7654b57b5c2
pryority <matthewapryor@gmail.com>
2022-12-14 14:31:46 -0500

    update image and colors

--- components/Farms.tsx
+++ components/Farms.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import farms from '../farms.json'
 import rpLogo from '../public/rp-logo.png'
 import ahLogo from '../public/ah-logo.png'
+import hopLogo from '../public/hop-logo.png';
 import nftx from '../public/nftx.png'
 
 type Props = {}
@@ -28,14 +29,16 @@ export const Farms = () => {
             <div key={i} className="flex relative p-2">
               <Link
               href={`${farm.url}`}
-              className={`${farm.name == "Rocket Pool" ? 'hover:bg-orange-500/40':''} ${farm.name == "Alpha Homora V2" ? 'hover:bg-blue-400/40 hover:text-sky-900':''} ${farm.name == "NFTx" ? 'hover:bg-red-400/40 visited:text-red-600 text-red-500 hover:text-red-500':''} flex h-[200px] justify-center flex-col w-56 transition-all ease-in-out duration-100 visited:text-sky-900 bg-lime-50-500 hover:bg-lime-300 rounded-xl border p-6 text-left hover:text-lime-900 focus:text-lime-50 focus:bg-lime-500 relative overflow-clip`}
+              target="_blank"
+              className={`${farm.name == "Rocket Pool" ? 'hover:bg-orange-500/40 focus:bg-orange-500':''} ${farm.name == "Hop Exchange" ? 'hover:bg-pink-500/40 focus:bg-pink-500':''} ${farm.name == "Alpha Homora V2" ? 'hover:bg-blue-400/40 hover:text-sky-900 focus:bg-blue-400':''} ${farm.name == "NFTx" ? 'hover:bg-red-400/40 focus:bg-red-400 visited:text-red-600 text-red-500 hover:text-red-500':''} flex h-[200px] justify-center flex-col w-56 transition-all ease-in-out duration-100 visited:text-sky-900 bg-lime-50-500 hover:bg-lime-300 rounded-xl border p-6 text-left hover:text-lime-900 focus:text-lime-50 focus:bg-lime-500 relative overflow-clip`}
               >
                 {farm.name == "Rocket Pool" ? <Image src={rpLogo} alt="" className="object-fill scale-[2] md:scale-[3] opacity-50" /> : ""}
+                {farm.name == "Hop Exchange" ? <Image src={hopLogo} alt="" className="object-fill scale-[2] md:scale-[2] opacity-50" /> : ""}
                 {farm.name == "Alpha Homora V2" ? <Image src={ahLogo} alt="" className="object-fill scale-[2] md:scale-[3] opacity-25" /> : ""}
-                {farm.name == "NFTx" ? <Image src={nftx} alt="" className="object-fill scale-[3.2] md:scale-[3] opacity-25" /> : ""}
-                <div className={`${farm.name === "Rocket Pool" ? 'absolute text-orange-900' : "absolute"} ${farm.name === "Alpha Homora V2" ? 'absolute text-blue-900 max-w-[60%] sm:max-w-[70%]' : "absolute"}`}>
+                {farm.name == "NFTx" ? <Image src={nftx} alt="" className="object-fill scale-[3] sm:scale-3 md:scale-[4] opacity-25" /> : ""}
+                <div className={`${farm.name === "Rocket Pool" ? 'absolute text-orange-900' : "absolute"} ${farm.name === "Hop Exchange" ? 'absolute text-pink-900' : "absolute"} ${farm.name === "NFTx" ? 'absolute text-red-900 max-w-[60%] sm:max-w-[70%]' : "absolute"} ${farm.name === "Alpha Homora V2" ? 'absolute text-blue-900 max-w-[60%] sm:max-w-[70%]' : "absolute"}`}>
                   <h3 className="text-2xl font-bold">{farm.name}</h3>
-                  <h2 className={`${farm.name === "Alpha Homora V2" ? "bg-blue-400 text-white":""} ${farm.name === "NFTx" ? "bg-red-400 text-white" : ""} ${farm.name === "Rocket Pool" ? "bg-orange-600 text-white":""} ${i > 2 ? "bg-slate-200" :""} text-xs p-1 rounded-lg w-fit`}>{farm.symbol}</h2>
+                  <h2 className={`${farm.name === "Hop Exchange" ? "bg-pink-400 text-white":""} ${farm.name === "Alpha Homora V2" ? "bg-blue-400 text-white":""} ${farm.name === "NFTx" ? "bg-red-800 text-white" : ""} ${farm.name === "Rocket Pool" ? "bg-orange-600 text-white":""} ${i > 2 ? "bg-slate-200" :""} text-xs p-1 rounded-lg w-fit`}>{farm.symbol}</h2>
                 </div>
               </Link>
             </div>
